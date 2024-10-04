d12e240ceeebf771396bb69bef22b87d253b5c70
Okenamay <yamabaka@yandex.ru>
2024-10-04 20:28:35 +0300

    Iteration 1
    Implemented: main.go
    Implemented: parcel.go
    Implemented: main_test.go
    
    All tests running OK.

--- parcel.go
+++ parcel.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 )
 
 type ParcelStore struct {
@@ -13,48 +14,125 @@ func NewParcelStore(db *sql.DB) ParcelStore {
 }
 
 func (s ParcelStore) Add(p Parcel) (int, error) {
-	// реализуйте добавление строки в таблицу parcel, используйте данные из переменной p
 
-	// верните идентификатор последней добавленной записи
-	return 0, nil
+	// Добавление строки в таблицу parcel с использованием данных из переменной p:
+	res, err := s.db.Exec("INSERT INTO parcel (client, status, address, created_at) VALUES (:client, :status, :address, :created_at)",
+		sql.Named("client", p.Client),
+		sql.Named("status", p.Status),
+		sql.Named("address", p.Address),
+		sql.Named("created_at", p.CreatedAt))
+	if err != nil {
+		return 0, err
+	}
+
+	// Определение идентификатора последней добавленной строки:
+	id, err := res.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	// Вывод идентификатора последней добавленной строки:
+	return int(id), nil // LastInsertId() выдаёт int64, пришлось конвертировать:
 }
 
 func (s ParcelStore) Get(number int) (Parcel, error) {
-	// реализуйте чтение строки по заданному number
-	// здесь из таблицы должна вернуться только одна строка
 
-	// заполните объект Parcel данными из таблицы
+	// Чтение одной строки из таблицы parcel по заданному number:
 	p := Parcel{}
+	row := s.db.QueryRow("SELECT number, client, status, address, created_at FROM parcel WHERE number = :number",
+		sql.Named("number", number))
+	err := row.Scan(&p.Number, &p.Client, &p.Status, &p.Address, &p.CreatedAt)
 
-	return p, nil
+	// Отладка:
+	if err != nil {
+		// Отладка:
+		fmt.Println("Ошибка в функции Get:", err)
+	}
+
+	return p, err
 }
 
 func (s ParcelStore) GetByClient(client int) ([]Parcel, error) {
-	// реализуйте чтение строк из таблицы parcel по заданному client
-	// здесь из таблицы может вернуться несколько строк
 
-	// заполните срез Parcel данными из таблицы
+	// Чтение строк из таблицы parcel по заданному client
+	rows, err := s.db.Query("SELECT * FROM parcel WHERE client = :client",
+		sql.Named("client", client))
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	// Заполнение среза []Parcel данными из таблицы:
 	var res []Parcel
 
-	return res, nil
+	for rows.Next() {
+		p := Parcel{}
+		err := rows.Scan(&p.Number, &p.Client, &p.Status, &p.Address, &p.CreatedAt)
+		if err != nil {
+			fmt.Println("Ошибка в функции GetByClient:", err)
+			return nil, err
+		}
+
+		res = append(res, p)
+	}
+
+	return res, err
 }
 
 func (s ParcelStore) SetStatus(number int, status string) error {
-	// реализуйте обновление статуса в таблице parcel
 
-	return nil
+	// Обновление статуса в таблице parcel:
+	_, err := s.db.Exec("UPDATE parcel SET status = :status WHERE number = :number",
+		sql.Named("status", status),
+		sql.Named("number", number))
+
+	return err
 }
 
 func (s ParcelStore) SetAddress(number int, address string) error {
-	// реализуйте обновление адреса в таблице parcel
-	// менять адрес можно только если значение статуса registered
 
-	return nil
+	// Обновление адреса в таблице parcel:
+	// Проверим, имеет ли статус записи с номером number значение registered:
+	p := Parcel{}
+	row := s.db.QueryRow("SELECT status FROM parcel WHERE number = :number",
+		sql.Named("number", number))
+	err := row.Scan(&p.Status)
+	if err != nil {
+		// Отладка:
+		fmt.Println("Ошибка в функции SetAddress:", err)
+		return err
+	}
+
+	// Если условние соблюдено, обновим адрес в таблице parcel:
+	if p.Status == ParcelStatusRegistered {
+		_, err := s.db.Exec("UPDATE parcel SET address = :address WHERE number = :number",
+			sql.Named("address", address),
+			sql.Named("number", number))
+		return err
+	}
+
+	return err
 }
 
 func (s ParcelStore) Delete(number int) error {
-	// реализуйте удаление строки из таблицы parcel
-	// удалять строку можно только если значение статуса registered
+	// Удаление строки из таблицы parcel:
+	// Проверим, имеет ли статус записи с номером number значение registered:
+	p := Parcel{}
+	rows := s.db.QueryRow("SELECT status FROM parcel WHERE number = :number",
+		sql.Named("number", number))
+	err := rows.Scan(&p.Status)
+	if err != nil {
+		// Отладка:
+		fmt.Println("Ошибка в функции Delete:", err)
+		return err
+	}
+
+	// Если условние соблюдено, удалим строку в таблице parcel:
+	if p.Status == ParcelStatusRegistered {
+		_, err := s.db.Exec("DELETE FROM parcel WHERE number = :number",
+			sql.Named("number", number))
+		return err
+	}
 
 	return nil
 }
--- parcel_test.go
+++ parcel_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -31,58 +32,103 @@ func getTestParcel() Parcel {
 // TestAddGetDelete проверяет добавление, получение и удаление посылки
 func TestAddGetDelete(t *testing.T) {
 	// prepare
-	db, err := // настройте подключение к БД
+	db, err := sql.Open("sqlite", "tracker.db")
+	require.NoError(t, err, "Ошибка подключения к БД.")
+	defer db.Close()
+
 	store := NewParcelStore(db)
 	parcel := getTestParcel()
 
 	// add
 	// добавьте новую посылку в БД, убедитесь в отсутствии ошибки и наличии идентификатора
+	id, err := store.Add(parcel)
+	require.NoError(t, err, "Ошибка добавления новой посылки.")
+	assert.NotEmpty(t, id, "Отсутствует идентификатор новой посылки.")
 
 	// get
 	// получите только что добавленную посылку, убедитесь в отсутствии ошибки
 	// проверьте, что значения всех полей в полученном объекте совпадают со значениями полей в переменной parcel
+	resParcel, err := store.Get(id)
+	require.NoError(t, err, "Ошибка получения данных посылки.")
+	parcel.Number = id
+	assert.Equal(t, parcel, resParcel, "Полученные данные отличаются от эталона: ID.")
 
 	// delete
 	// удалите добавленную посылку, убедитесь в отсутствии ошибки
 	// проверьте, что посылку больше нельзя получить из БД
+	err = store.Delete(id)
+	require.NoError(t, err, "Ошибка удаления посылки.")
+	_, err = store.Get(id)
+	require.Error(t, err, "Удаление посылки не произошло.")
+	require.Equal(t, sql.ErrNoRows, err, "Посылка не была удалена.")
 }
 
 // TestSetAddress проверяет обновление адреса
 func TestSetAddress(t *testing.T) {
 	// prepare
-	db, err := // настройте подключение к БД
+	db, err := sql.Open("sqlite", "tracker.db")
+	require.NoError(t, err, "Ошибка подключения к БД.")
+	defer db.Close()
+
+	store := NewParcelStore(db)
+	parcel := getTestParcel()
 
 	// add
 	// добавьте новую посылку в БД, убедитесь в отсутствии ошибки и наличии идентификатора
+	id, err := store.Add(parcel)
+	require.NoError(t, err, "Ошибка добавления новой посылки.")
+	assert.NotEmpty(t, id, "Отсутствует идентификатор новой посылки.")
 
 	// set address
 	// обновите адрес, убедитесь в отсутствии ошибки
 	newAddress := "new test address"
+	err = store.SetAddress(id, newAddress)
+	require.NoError(t, err, "Ошибка смены статуса посылки.")
 
 	// check
 	// получите добавленную посылку и убедитесь, что адрес обновился
+	checkParcel, err := store.Get(id)
+	require.NoError(t, err, "Ошибка получения данных обновлённой посылки.")
+	assert.Equal(t, newAddress, checkParcel.Address, "Ошибка записи нового адреса посылки.")
 }
 
 // TestSetStatus проверяет обновление статуса
 func TestSetStatus(t *testing.T) {
 	// prepare
-	db, err := // настройте подключение к БД
+	db, err := sql.Open("sqlite", "tracker.db")
+	require.NoError(t, err, "Ошибка подключения к БД.")
+	defer db.Close()
+
+	store := NewParcelStore(db)
+	parcel := getTestParcel()
 
 	// add
 	// добавьте новую посылку в БД, убедитесь в отсутствии ошибки и наличии идентификатора
+	id, err := store.Add(parcel)
+	require.NoError(t, err, "Ошибка добавления новой посылки.")
+	require.NotEmpty(t, id, "Отсутствует идентификатор новой посылки.")
 
 	// set status
 	// обновите статус, убедитесь в отсутствии ошибки
+	newStatus := "In outer space!"
+	err = store.SetStatus(id, newStatus)
+	require.NoError(t, err, "Ошибка смены статуса посылки.")
 
 	// check
 	// получите добавленную посылку и убедитесь, что статус обновился
+	checkParcel, err := store.Get(id)
+	require.NoError(t, err, "Ошибка получения данных обновлённой посылки.")
+	assert.Equal(t, newStatus, checkParcel.Status, "Ошибка записи нового статуса посылки.")
 }
 
 // TestGetByClient проверяет получение посылок по идентификатору клиента
 func TestGetByClient(t *testing.T) {
 	// prepare
-	db, err := // настройте подключение к БД
+	db, err := sql.Open("sqlite", "tracker.db")
+	require.NoError(t, err, "Ошибка подключения к БД.")
+	defer db.Close()
 
+	store := NewParcelStore(db)
 	parcels := []Parcel{
 		getTestParcel(),
 		getTestParcel(),
@@ -98,7 +144,9 @@ func TestGetByClient(t *testing.T) {
 
 	// add
 	for i := 0; i < len(parcels); i++ {
-		id, err := // добавьте новую посылку в БД, убедитесь в отсутствии ошибки и наличии идентификатора
+		id, err := store.Add(parcels[i]) // добавьте новую посылку в БД, убедитесь в отсутствии ошибки и наличии идентификатора
+		require.NoError(t, err, "Ошибка добавления новой посылки номер %i.", i)
+		require.NotEmpty(t, id, "Отсутствует идентификатор новой посылки %i.", i)
 
 		// обновляем идентификатор добавленной у посылки
 		parcels[i].Number = id
@@ -108,14 +156,20 @@ func TestGetByClient(t *testing.T) {
 	}
 
 	// get by client
-	storedParcels, err := // получите список посылок по идентификатору клиента, сохранённого в переменной client
+	// получите список посылок по идентификатору клиента, сохранённого в переменной client
 	// убедитесь в отсутствии ошибки
 	// убедитесь, что количество полученных посылок совпадает с количеством добавленных
+	storedParcels, err := store.GetByClient(client)
+	require.NoError(t, err, "Ошибка получения списка посылок по ID клиента.")
+	assert.Len(t, parcels, len(storedParcels), "Неверное количество посылок для клиента.")
 
 	// check
 	for _, parcel := range storedParcels {
 		// в parcelMap лежат добавленные посылки, ключ - идентификатор посылки, значение - сама посылка
 		// убедитесь, что все посылки из storedParcels есть в parcelMap
 		// убедитесь, что значения полей полученных посылок заполнены верно
+		parcy, ok := parcelMap[parcel.Number]
+		require.True(t, ok, "Такой посылки не существует.")
+		assert.Equal(t, parcy, parcel, "Не совпадают данные посылки.")
 	}
 }
